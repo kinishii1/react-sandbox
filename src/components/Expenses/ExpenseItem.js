7f90a023ad12d342c77d70b6809bdc92377189ef
Kin Ishii <kinishii97@gmail.com>
2023-09-01 22:55:51 +0900

    project finished feat. add a dinamic graphic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,21 @@
-import { useState } from "react";
-import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css";
+import React from 'react';
 
-function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title)
-  const clickHandler = () => {
-    setTitle('Updated')
-  } 
-  return  (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </div>
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  return (
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
